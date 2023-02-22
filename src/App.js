7c42063f3c1833247a13b7bf9d5938c4dd7016c9
saikrishnatbijil <saisboxintech@hmail.com>
2023-02-22 20:51:16 +0530

    fixing....

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 import { Home, ContactUs } from './pages'
-import { Route, Routes } from 'react-router-dom';
-import { Navbar } from './components';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contactus' element={<ContactUs />} />
-      </Routes>
+      <Home /> 
+      <ContactUs />
     </>
   );
 }
