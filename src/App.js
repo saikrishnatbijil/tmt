793690be8b667866adaf7e22d5b9ec23b8888ea0
saikrishnatbijil <saisboxintech@hmail.com>
2023-02-22 21:04:14 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { Home, ContactUs } from './pages'
+import { Route, Routes } from 'react-router-dom';
+import { Navbar } from './components';
 
 function App() {
   return (
     <>
-      <Home /> 
-      <ContactUs />
+      <Routes>
+        <Route path='/tmt/' element={<Home />} />
+        <Route path='/tmt/contactus' element={<ContactUs />} />
+      </Routes>
     </>
   );
 }
